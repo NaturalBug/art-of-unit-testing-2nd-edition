7fb31764229cfc54333ed951cfdf416a7012ed80
Cheng-Ying Li <newtaiwanstar@gmail.com>
2021-11-20 18:33:48 +0800

    feat: add IsVaildLogFileName

---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsVaildLogFileName(string fileName)
+        {
+            if (fileName.EndsWith(".SLF"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
