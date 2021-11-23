5d2de32751ca0b3482d3d4ed433722d540406590
chengying.li <chengying.li@bridgewell.com>
2021-11-23 20:34:15 +0800

    test: add the other test case

--- MemCalculator.UnitTests/MemCalculatorTests.cs
+++ MemCalculator.UnitTests/MemCalculatorTests.cs
@@ -4,14 +4,23 @@ namespace MemCalculator.UnitTests
 {
     public class MemCalculatorTests
     {
+        private readonly MemCalculator calc = new();
+
         [Fact]
         public void Sum_ByDefault_ReturnsZero()
         {
-            MemCalculator calc = new();
-
             int lastSum = calc.Sum();
 
             Assert.Equal(0, lastSum);
+        }
+
+        [Fact]
+        public void Add_WenCalled_ChangesSum()
+        {
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.Equal(1, sum);
         }
     }
 }
--- MemCalculator.nUnitTests/MemCalculatorTests.cs
+++ MemCalculator.nUnitTests/MemCalculatorTests.cs
@@ -8,11 +8,27 @@ namespace MemCalculator.nUnitTests
         [Test]
         public void Sum_ByDefault_ReturnsZero()
         {
-            MemCalculator calc = new();
+            MemCalculator calc = MakeCalc();
 
             int lastSum = calc.Sum();
 
             Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = MakeCalc();
+
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
         }
     }
 }
