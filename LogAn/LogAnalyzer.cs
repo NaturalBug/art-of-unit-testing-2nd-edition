bd40c6fcf737d2c8b9c7bee437ddcb09bef5cf9e
Cheng-Ying Li <newtaiwanstar@gmail.com>
2021-12-05 12:25:28 +0800

    refactor: extract class to read file

--- LogAn/LogAnalyzer.cs
+++ LogAn/LogAnalyzer.cs
@@ -1,27 +1,21 @@
-﻿using System;
-
-namespace LogAn
+﻿namespace LogAn
 {
     public class LogAnalyzer
     {
         public bool WasLastFileNameValid { get; set; }
 
+
         public bool IsVaildLogFileName(string fileName)
         {
+            FileExtensionManager mgr = new FileExtensionManager();
+            if (mgr.IsValid(fileName))
+            {
+                WasLastFileNameValid = true;
+                return true;
+            }
+
             WasLastFileNameValid = false;
-
-            if (string.IsNullOrEmpty(fileName))
-            {
-                throw new ArgumentException("filename has to be provided");
-            }
-
-            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
-            {
-                return false;
-            }
-
-            WasLastFileNameValid = true;
-            return true;
+            return false;
         }
     }
 }
